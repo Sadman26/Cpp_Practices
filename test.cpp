08f533b7fcba3e885144daa362df423f52c0848d
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-10-22 06:48:40 +0600

    Simple boolean operation

--- test.cpp
+++ test.cpp
@@ -2,25 +2,8 @@
 using namespace std;
 int main()
 {
-    //maximum
-    cout<<max(2,3)<<endl;
-    //minimum
-    cout<<min(2,3)<<endl;
-    //square root of 64
-    cout<<sqrt(64)<<endl;
-    //Round of value 2.3
-    cout<<round(2.3)<<endl;
-    //Floor value of 2.3
-    cout<<floor(2.3)<<endl;
-    //Ceil value of 2.3
-    cout<<ceil(2.3)<<endl;
-    //exponent of 2
-    cout<<exp(2)<<endl;
-    //sin value
-    cout<<sin(45)<<endl;
-    //cos value
-    cout<<cos(30)<<endl;
-    //tan value
-    cout<<tan(30)<<endl;
-    
+    int x=10;
+    int y=20;
+    //returns 1 it means it is true or else false
+    cout<<(x<y);
 }
